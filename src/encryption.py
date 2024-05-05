951cae74f064effe60c7bb22509c444d7a7fd39c
ProNoice <robex.omar@gmail.com>
2024-05-05 08:27:05 +0300

    Added new decryption method

--- src/encryption.py
+++ src/encryption.py
@@ -1,6 +1,7 @@
 # For password encryption when I add that feature
 
 import os
+import json
 
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
@@ -8,32 +9,42 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
+import requests
+from requests import Response
+
 salt = b"=\x9a\xcc\xe8\xd2\x9c\x97c\x97?(|\xaa\xd1\x98S)\xe8\x9cL\xed\x90\xa6\xc2\x07\xb5\x9d\xf7C=\xf7\x87\x11SHR\xcaou\xe2\x10\x91\xadN\x17'\x1b\xc7v3\x18\xbc\xa0?X\xbe\xb7X\xa5\xf6\xf3\xdf\xac\xa7"
-password = os.getenv('EncryptionKey') or "werfgjiklohui4f5toyj9534ft"
-print(password)
+password = os.getenv("EncryptionKey") or "werfgjiklohui4f5toyj9534ft"
 key = PBKDF2(password, salt, dkLen=32)
+
 
 def encrypt(filename, message):
 
     cipher = AES.new(key, AES.MODE_CBC)
     ciphered_data = cipher.encrypt(pad(message, AES.block_size))
 
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(cipher.iv)
         f.write(ciphered_data)
 
     return ciphered_data
 
+
 def decrypt(filename):
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         iv = f.read(16)
         decrypt_data = f.read()
 
-    #cipher = AES.new(key, AES.MODE_CBC, iv=iv)
-    #original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
+    # cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    # original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
     decrypted_password = cipher.decrypt(decrypt_data)
     unpadded_password = unpad(decrypted_password, AES.block_size)
-    password = unpadded_password.decode('utf-8')
+    password = unpadded_password.decode("utf-8")
 
     return password
+
+
+def decrypt_response_content(response: Response):
+    response.apparent_encoding
+    parsed = json.loads(response.content.decode(response.encoding))
+    return parsed
--- src/main.py
+++ src/main.py
@@ -12,12 +12,14 @@ load_dotenv(find_dotenv())
 
 app = FastAPI()
 
+
 def check_password(input):
-    if decrypt('../auth.bin') == input:
+    if decrypt("../auth.bin") == input:
         return True
     return False
 
-@app.get('/')
+
+@app.get("/")
 def is_online():
     """
     Checks if the PyDBConnect Database server is online.
@@ -26,106 +28,154 @@ def is_online():
 
     Returns: HTTPException
     """
-    raise HTTPException(status_code=status.HTTP_200_OK, detail='PyDBConnect Database Server is online!')
+    raise HTTPException(
+        status_code=status.HTTP_200_OK, detail="PyDBConnect Database Server is online!"
+    )
 
-# Returns collections of specified project
-@app.get('/get/{project}/{password}')
+
+@app.get("/get/{project}/{password}")
 def get_project(project: str, password: str):
     """
     Returns all collections from specified project
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
     if os.path.exists(project_dir):
         return get_collections(project_dir)
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project not found!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found!"
+        )
 
-#returns specified collection of project.
-@app.get('/get/{project}/{collection}/{password}')
+
+@app.get("/get/{project}/{collection}/{password}")
 def get_collection(project: str, collection: str, password: str):
     """
-    Returns all data in the specified collection 
+    Returns all data in the specified collection
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        return json.load(open(project_dir + collection + '.json'))
-        
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        return json.load(open(project_dir + collection + ".json"))
 
-#returns specified document of collection.
-@app.get('/get/{project}/{collection}/{document}/{password}')
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Project or collection not found!"
+    )
+
+
+@app.get("/get/{project}/{collection}/{document}/{password}")
 def get_document(project: str, collection: str, document: str, password: str):
     """
     Returns the data of a document
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        data = json.load(open(project_dir + collection + '.json'))
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        data = json.load(open(project_dir + collection + ".json"))
         if document in data:
             return data[document]
         else:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Document not found in collection!')
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Document not found in collection!",
+            )
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
-    
-#creates a new document in specified collection
-@app.post('/create-document/{project}/{collection}/{document_id}/{password}')
-def create_document(project: str, collection: str, document_id: str, document: dict, password: str):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project or collection not found!",
+        )
+
+
+@app.post("/create-document/{project}/{collection}/{document_id}/{password}")
+def create_document(
+    project: str, collection: str, document_id: str, document: dict, password: str
+):
     """
     Creates a document with the specified id in the specified collection
-    
+
     Returns: document data
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        current_collection = json.load(open(project_dir + collection + '.json'))
-        
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        current_collection = json.load(open(project_dir + collection + ".json"))
+
         if document_id in current_collection:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='specified document_id not found in collection!')
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="specified document_id not found in collection!",
+            )
 
         current_collection[document_id] = document
 
-        with open(project_dir + collection + '.json', 'w') as file:
+        with open(project_dir + collection + ".json", "w") as file:
             json.dump(current_collection, file, indent=4)
 
         return current_collection[document_id]
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project or collection not found!",
+        )
 
-@app.put('/update-document/{project}/{collection}/{document_id}/{password}')
-def update_document(project: str, collection: str, document_id: str, document: dict, password: str):
+
+@app.put("/update-document/{project}/{collection}/{document_id}/{password}")
+def update_document(
+    project: str, collection: str, document_id: str, document: dict, password: str
+):
     """
     Changes data of the specified document
 
     Returns: the new data of the document
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        current_collection = json.load(open(project_dir + collection + '.json'))
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        current_collection = json.load(open(project_dir + collection + ".json"))
 
         if not document_id in current_collection:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='specified document_id not found in collection!')
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="specified document_id not found in collection!",
+            )
 
         current_collection[document_id] = document
 
-        with open(project_dir + collection + '.json', 'w') as file:
+        with open(project_dir + collection + ".json", "w") as file:
             json.dump(current_collection, file, indent=4)
 
         return current_collection[document_id]
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
-    
-@app.delete('/delete-document/{project}/{collection}/{document_id}/{password}')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project or collection not found!",
+        )
+
+
+@app.delete("/delete-document/{project}/{collection}/{document_id}/{password}")
 def delete_document(project: str, collection: str, document_id: str, password: str):
     """
     Deletes the specified document from the collection
@@ -133,22 +183,33 @@ def delete_document(project: str, collection: str, document_id: str, password: s
     Returns: None
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        current_collection = json.load(open(project_dir + collection + '.json'))
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        current_collection = json.load(open(project_dir + collection + ".json"))
 
         if not document_id in current_collection:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='specified document_id not found in collection!')
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="specified document_id not found in collection!",
+            )
 
         del current_collection[document_id]
 
-        with open(project_dir + collection + '.json', 'w') as file:
+        with open(project_dir + collection + ".json", "w") as file:
             json.dump(current_collection, file, indent=4)
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project or collection not found!",
+        )
 
-@app.post('/create-collection/{project}/{collection}/{password}')
+
+@app.post("/create-collection/{project}/{collection}/{password}")
 def create_collection(project: str, collection: str, password: str):
     """
     Creates a new empty collection in the specified project
@@ -156,18 +217,26 @@ def create_collection(project: str, collection: str, password: str):
     returns: None
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
     if os.path.exists(project_dir):
-        if os.path.exists(project_dir + collection + '.json'):
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Collection already exists!')
-        
-        with open(project_dir + collection + '.json', 'w') as file:
+        if os.path.exists(project_dir + collection + ".json"):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Collection already exists!",
+            )
+
+        with open(project_dir + collection + ".json", "w") as file:
             json.dump({}, file, indent=4)
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project not found!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Project not found!"
+        )
 
-@app.delete('/delete-collection/{project}/{collection}/{password}')
+
+@app.delete("/delete-collection/{project}/{collection}/{password}")
 def delete_collection(project: str, collection: str, password: str):
     """
     Deletes the specified collection from the project
@@ -175,9 +244,16 @@ def delete_collection(project: str, collection: str, password: str):
     Returns: None
     """
     if not check_password(password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!")
-    project_dir = os.getcwd() + '/../data/' + project + '/'
-    if os.path.exists(project_dir) and os.path.exists(project_dir + collection + '.json'):
-        os.remove(project_dir + collection + '.json')
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password!"
+        )
+    project_dir = os.getcwd() + "/../data/" + project + "/"
+    if os.path.exists(project_dir) and os.path.exists(
+        project_dir + collection + ".json"
+    ):
+        os.remove(project_dir + collection + ".json")
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Project or collection not found!')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project or collection not found!",
+        )
